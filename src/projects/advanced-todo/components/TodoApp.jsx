7163d682745c61f0a37059a871856d52482d4fbd
Abu-05 <pinjariabubakar2005@gmail.com>
2025-12-15 21:46:49 +0530

    Add filter functionality to advanced todo app

--- src/projects/advanced-todo/components/TodoApp.jsx
+++ src/projects/advanced-todo/components/TodoApp.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 import { todoReducer } from "../reducer/todoReducer";
 import TodoInput from "./TodoInput";
 import TodoList from "./TodoList";
@@ -6,12 +6,28 @@ import styles from "../styles/todo.module.css";
 
 function TodoApp() {
   const [todos, dispatch] = useReducer(todoReducer, []);
+  const [filter, setFilter] = useState("ALL");
+
+  const filteredTodos = todos.filter(todo => {
+    if (filter === "ACTIVE") return !todo.completed;
+    if (filter === "COMPLETED") return todo.completed;
+    return true; // ALL
+  });
 
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Advanced Todo App</h2>
+
       <TodoInput dispatch={dispatch} />
-      <TodoList todos={todos} dispatch={dispatch} />
+
+      {/* Filter Buttons */}
+      <div style={{ marginBottom: "15px" }}>
+        <button onClick={() => setFilter("ALL")}>All</button>
+        <button onClick={() => setFilter("ACTIVE")}>Active</button>
+        <button onClick={() => setFilter("COMPLETED")}>Completed</button>
+      </div>
+
+      <TodoList todos={filteredTodos} dispatch={dispatch} />
     </div>
   );
 }
